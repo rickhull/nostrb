d666ec488823b6a82a417a4d984969bc71afcf1d
Rick Hull <rick.hull@gmail.com>
2024-09-06 15:36:44 -0400

    remove @buffer

--- lib/nostrb/relay.rb
+++ lib/nostrb/relay.rb
@@ -14,64 +14,63 @@ module Nostr
     def self.message(excp) = format("%s: %s", excp.class, excp.message)
     def self.error(e) = notice(message(e))
 
-    attr_reader :events, :subs
+    attr_reader :events
 
     def initialize
       @events = [] # all events accepted
-      @buffer = []
     end
 
     # accepts a single json array
-    # returns a ruby array of json arrays
+    # returns a ruby array of response strings (json array)
     def ingest(json)
       a = Nostr.ary!(Nostr.parse(json))
       case a[0]
       when 'EVENT'
-        handle_event(Nostr.check!(a[1], Hash))
+        [handle_event(Nostr.check!(a[1], Hash))]
       when 'REQ'
         sid = Nostr.sid!(a[1])
         filters = a[2..-1].map { |f| Filter.ingest(f) }
         handle_req(sid, *filters)
       when 'CLOSE'
-        handle_close(Nostr.sid!(a[1]))
+        [handle_close(Nostr.sid!(a[1]))]
       else
         raise 'unexpected'
       end
-      buffer, @buffer = @buffer, []
-      buffer.map { |a| Nostr.json a }
     end
 
+    # return a single response
     def handle_event(hsh)
       begin
         hsh = SignedEvent.validate!(hsh)
         eid = hsh.fetch("id")
         @events << hsh
-        @buffer << Server.ok(eid)
+        Server.ok(eid)
       rescue SignedEvent::Error => e
-        @buffer << Server.ok(eid, Server.message(e), ok: false)
+        Server.ok(eid, Server.message(e), ok: false)
       rescue Nostr::Error, KeyError => e
-        @buffer << Server.error(e)
+        Server.error(e)
       rescue RuntimeError => e
-        @buffer << Server.error(e)
+        Server.error(e)
       end
     end
 
+    # return an array of response
     def handle_req(sid, *filters)
-      results = []
+      responses = []
       @events.each { |e|
         match = false
         filters.each { |f|
           next if match
           match = f.match?(e)
         }
-        results << e if match
+        responses << Server.event(sid, e) if match
       }
-      results.each { |e| @buffer << Server.event(sid, e) }
-      @buffer << Server.eose(sid)
+      responses << Server.eose(sid)
     end
 
+    # single response
     def handle_close(sid)
-      @buffer << Server.closed(sid, "CLOSE requested")
+      Server.closed(sid, "CLOSE requested")
     end
   end
 end
