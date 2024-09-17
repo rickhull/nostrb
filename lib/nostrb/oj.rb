fe3a5130ecd49c961e064b2927a886ff1f43ed2f
Rick Hull <rick.hull@gmail.com>
2024-09-17 16:49:56 -0400

    just use mode: :strict

--- lib/nostrb/oj.rb
+++ lib/nostrb/oj.rb
@@ -1,15 +1,6 @@
 require 'oj'
 
 module Nostrb
-  # per NIP-01
-  Oj.default_options = {
-    nan: :raise,        # raise on NaN / Infinity
-    indent: 0,          # full squish
-    safe: true,         # limit size of ParseErrors
-    escape_mode: :json, # default, not ASCII- or XSS-safe
-    max_nesting: 4,     # sanity check; wire format is 4 deep
-  }
-
-  def self.parse(json)  = Oj.load(json)
-  def self.json(object) = Oj.dump(object)
+  def self.parse(json)  = Oj.load(json, mode: :strict)
+  def self.json(object) = Oj.dump(object, mode: :strict)
 end
