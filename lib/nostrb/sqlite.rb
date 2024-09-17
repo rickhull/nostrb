4a7852ae95e3d855cbe250843df981e59d1e96dd
Rick Hull <rick.hull@gmail.com>
2024-09-17 13:27:18 -0400

    handle pragmas directly

--- lib/nostrb/sqlite.rb
+++ lib/nostrb/sqlite.rb
@@ -2,6 +2,109 @@ require 'sqlite3'
 
 module Nostrb
   class Storage
+    class Pragma
+      ENUM = {
+        'auto_vacuum' => %w[none full incremental],
+        'synchronous' => %w[off normal full],
+        'temp_store' => %w[default file memory],
+      }
+      READONLY = %w[data_version freelist_count page_count]
+      SCALAR =
+        %w[application_id analysis_limit auto_vacuum automatic_index
+           busy_timeout cache_size cache_spill cell_size_check
+           checkpoint_fullfsync defer_foreign_keys encoding foreign_keys
+           fullfsync hard_heap_limit ignore_check_constraints journal_mode
+           journal_size_limit locking_mode max_page_count mmap_size page_size
+           query_only read_uncommitted recursive_triggers
+           reverse_unordered_selects secure_delete soft_heap_limit synchronous
+           temp_store threads trusted_schema user_version wal_autocheckpoint]
+
+      # debug: parser_trace schema_version stats vdbe_* writable_schema
+      # legacy: legacy_alter_table legacy_file_format
+      # deprecated: case_sensitive_like count_changes data_store_directory
+      #             default_cache_size empty_result_callbacks full_column_names
+      #             short_column_names temp_store_directory
+
+      REPORT = {
+        compile_options: nil,
+        # list
+        collation_list: nil,
+        database_list: nil,
+        function_list: nil,
+        module_list: nil,
+        pragma_list: nil,
+        table_list: nil,
+        index_list: :table_name,
+        foreign_key_list: :table_name,
+        # info
+        index_info: :index_name,
+        index_xinfo: :index_name,
+        table_info: :table_name,
+        table_xinfo: :table_name,
+      }
+
+      # either no args (nil) or a single arg (symbol)
+      COMMAND = {
+        # checks
+        foreign_key_check: :optional,  # report
+        integrity_check: :optional,    # ok
+        quick_check: :optional,        # ok
+        # manipulation
+        incremental_vacuum: :optional, # empty
+        optimize: :optional,           # empty
+        shrink_memory: nil,            # empty
+        wal_checkpoint: :optional,
+      }
+
+      def initialize(db)
+        @db = db
+      end
+
+      def get(pragma)
+        @db.execute("PRAGMA #{pragma}")[0][0]
+      end
+
+      def set(pragma, val)
+        @db.execute("PRAGMA #{pragma} = #{val}")
+        get(pragma)
+      end
+
+      def report(pragma, arg = nil)
+        if arg.nil?
+          @db.execute2("PRAGMA #{pragma}")
+        else
+          @db.execute2("PRAGMA #{pragma}(#{arg})")
+        end
+      end
+
+      def list(pragma, arg = nil)
+        if arg.nil?
+          @db.execute("PRAGMA #{pragma}")
+        else
+          @db.execute("PRAGMA #{pragma}(#{arg})")
+        end
+      end
+
+      READONLY.each { |pragma|
+        define_method(pragma) { get(pragma) }
+      }
+
+      SCALAR.each { |pragma|
+        define_method(pragma) { get(pragma) }
+        define_method(pragma + '=') { |val| set(pragma, val) }
+      }
+
+      (REPORT.merge(COMMAND)).each { |pragma, arg|
+        if arg.nil?
+          define_method(pragma) { report(pragma) }
+        elsif arg == :optional
+          define_method(pragma) { |val=nil| report(pragma, val) }
+        else
+          define_method(pragma) { |val| report(pragma, val) }
+        end
+      }
+    end
+
     KB = 1024
     MB = KB * 1024
     GB = MB * 1024
@@ -9,82 +112,9 @@ module Nostrb
     FILENAME = 'tmp.db'
     CONFIG = {
       default_transaction_mode: :immediate,
-      results_as_hash: true,
     }
 
-    # busy_timeout and ignore_check_constrants are added dynamically
-    PRAGMAS =
-      %w[application_id auto_vacuum automatic_index busy_timeout
-         cache_size cache_spill cell_size_check checkpoint_fullfsync
-         data_version defer_foreign_keys encoding foreign_keys
-         freelist_count fullfsync ignore_check_constraints journal_mode
-         journal_size_limit locking_mode max_page_count mmap_size page_count
-         page_size query_only read_uncommitted recursive_triggers
-         reverse_unordered_selects secure_delete soft_heap_limit synchronous
-         temp_store threads user_version wal_autocheckpoint wal_checkpoint]
-
-    # debug: parser_trace schema_version stats vdbe_* writable_schema
-    # legacy: legacy_alter_table legacy_file_format
-    # deprecated: case_sensitive_like count_changes data_store_directory
-    #             default_cache_size empty_result_callbacks full_column_names
-    #             short_column_names temp_store_directory
-    # assign only: busy_timeout= ignore_check_constraints=
-    # unsupported: analysis_limit hard_heap_limit trusted_schema
-
-    # either no args (nil) or a single arg (symbol)
-    COMMAND_PRAGMAS = {
-      collation_list: nil,
-      compile_options: nil,
-      database_list: nil,
-      foreign_key_check: nil,
-      function_list: nil,     # added dynamically
-      integrity_check: nil,
-      module_list: nil,       # added dynamically
-      optimize: nil,          # added dynamically
-      pragma_list: nil,       # added dynamically
-      quick_check: nil,
-      shrink_memory: nil,
-      table_list: nil,        # added dynamically
-      foreign_key_list: :table_name,
-      incremental_vacuum: :num_pages,
-      index_info: :index_name,
-      index_list: :table_name,
-      index_xinfo: :index_name,
-      table_info: :table_name,
-      table_xinfo: :table_name, # added dynamically
-    }
-
-    # add these below
-    DYNAMIC_PRAGMAS = {
-      busy_timeout: :scalar,
-      ignore_check_constraints: :scalar,
-      function_list: nil,
-      module_list: nil,
-      optimize: nil,
-      pragma_list: nil,
-      table_list: nil,
-      table_xinfo: :table_name,
-    }
-
-    DYNAMIC_PRAGMAS.each { |name, val|
-      if val
-        if val == :scalar
-          SQLite3::Database.define_method(name) {
-            execute("PRAGMA #{name}").first.values.first
-          }
-        else
-          SQLite3::Database.define_method(name) { |arg|
-            execute("PRAGMA #{name}(#{arg})")
-          }
-        end
-      else
-        SQLite3::Database.define_method(name) {
-          execute("PRAGMA #{name}")
-        }
-      end
-    }
-
-    SET_PRAGMAS = {
+    PRAGMAS = {
       foreign_keys: true,          # enable FK constrainsts
       mmap_size: 128 * MB,         # enable mmap I/O, 128 MB
 
@@ -95,104 +125,81 @@ module Nostrb
       wal_autocheckpoint: 1000,    # default, pages per fsync
     }
 
-    attr_reader :filename, :db
+    SQLITE_USAGE = /\Asqlite_/
+
+    attr_reader :filename, :db, :pragma
 
     def initialize(filename = FILENAME, **kwargs)
       @filename = filename
       @db = SQLite3::Database.new(@filename, **CONFIG.merge(kwargs))
       @db.busy_handler_timeout = 5000 # 5 seconds, release GVL every ms
-      self.class::SET_PRAGMAS.each { |name, val| @db.send("#{name}=", val) }
+      @pragma = Pragma.new(@db)
+      self.class::PRAGMAS.each { |name, val| @pragma.set(name, val) }
     end
 
-    def compile_options
-      @db.compile_options.map { |h| h['compile_options'] }
+    # below methods all return an array of strings
+    def compile_options = @pragma.list(:compile_options).map { |a| a[0] }
+    def database_files = @pragma.list(:database_list).map { |a| a[2] }
+    def all_table_names = @pragma.list(:table_list).map { |a| a[1] }
+
+    def table_names
+      all_table_names().select { |name| !SQLITE_USAGE.match name }
+    end
+
+    def all_index_names(table_name)
+      @pragma.list(:index_list, table_name).map { |a| a[1] }
+    end
+
+    def index_names(table_name)
+      all_index_names(table_name).select { |name| !SQLITE_USAGE.match name }
     end
 
     def report
-      auto_vacuum = %w[none full incremental]
-      synchronous = %w[off normal full]
-      temp_store = %w[default file memory]
-      wal_checkpoint = %w[passive full restart truncate]
-
-      lines = ["compile_options", '---']
+      lines = ['compile_options', '---']
       lines += self.compile_options
 
-      lines += ['', "database_list", '---']
-      lines += @db.database_list
+      lines += ['', 'database_files', '---']
+      lines += self.database_files
 
-      lines += ['', "table_list", '---']
-      tables = @db.table_list
+      lines += ['', "table_names", '---']
+      tables = self.table_names
       lines += tables
 
-      lines += ['', "table_info", '---']
-      my_tables = []
-      tables.each { |hsh|
-        tbl = hsh.fetch('name')
-        next if tbl.match /sqlite_(?:temp_)?schema/
-        my_tables << tbl
-        str = @db.table_info(tbl).inject("") { |memo, hsh|
-          col = format("%s:%s", hsh["name"], hsh["type"])
-          if hsh["pk"] > 0
-            col = format("%s:pk%i", col, hsh["pk"])
-          elsif hsh["notnull"] == 1
-            col = format("%s:notnull", col)
-          end
-          memo + format(" %s", col)
-        }
-        lines << format("%s:%s", tbl, str)
-      }
+      tables.each { |tbl|
+        lines += ['', "table_info(#{tbl})", '---']
+        lines += @pragma.table_info(tbl).map(&:inspect)
 
-      lines += ['', "foreign_key_list", '---']
-      my_tables.each { |tbl|
-        lines += @db.foreign_key_list(tbl).map { |h|
-          format("%s: %s", tbl, h)
+        fks = @pragma.foreign_key_list(tbl).map(&:inspect)
+        if fks.length > 1
+          lines += ['', "foreign_key_list(#{tbl})", '---']
+          lines += fks
+        end
+
+        idxs = self.index_names(tbl)
+        if !idxs.empty?
+          lines += ['', "index_names(#{tbl})", '---']
+          lines += idxs
+        end
+
+        idxs.each { |idx|
+          lines += ['', "index_info(#{idx})", '---']
+          lines += @pragma.index_info(idx).map(&:inspect)
         }
       }
 
-      lines += ['', "index_list", '---']
-      my_idx = []
-      my_tables.each { |tbl|
-        lines += @db.index_list(tbl).map { |h|
-          if !h.fetch("name").match /sqlite_autoindex/
-            my_idx << h["name"]
-          end
-          format("%s: %s", tbl, h)
-        }
-      }
-
-      lines += ['', "index_info", '---']
-      my_idx.each { |idx|
-        lines += @db.index_info(idx).map { |h|
-          format("%s: %s", idx, h)
-        }
-      }
-
-      lines += ['', "pragmas", '---']
-      PRAGMAS.each { |pragma|
-        val = case pragma
-              when 'auto_vacuum'
-                int = @db.auto_vacuum
-                format("%i (%s)", int, auto_vacuum[int])
-              when 'synchronous'
-                int = @db.synchronous
-                format("%i (%s)", int, synchronous[int])
-              when 'temp_store'
-                int = @db.temp_store
-                format("%i (%s)", int, temp_store[int])
-              when 'wal_checkpoint'
-                int = @db.wal_checkpoint
-                format("%i (%s)", int, wal_checkpoint[int])
-              else
-                @db.send(pragma)
-              end
+      lines += ['', "pragma valuess", '---']
+      Pragma::SCALAR.each { |pragma|
+        val, enum = @pragma.get(pragma), Pragma::ENUM[pragma]
+        val = format("%i (%s)", val, enum[val]) if enum
         lines << format("%s: %s", pragma, val)
       }
+
       lines
     end
   end
 
   class Reader < Storage
-    SET_PRAGMAS = Storage::SET_PRAGMAS.merge(query_only: true)
+    PRAGMAS = Storage::PRAGMAS.merge(query_only: true)
 
     def initialize(filename = FILENAME, **kwargs)
       super(filename, **kwargs.merge(readonly: true))
@@ -228,16 +235,6 @@ module Nostrb
   end
 
   class Writer < Storage
-    def prepare
-      @add_event =
-        @db.prepare("INSERT INTO events
-                          VALUES (:content, :kind, :pubkey,
-                                  :created_at, :id, :sig)")
-      @add_tag =
-        @db.prepare("INSERT INTO tags " +
-                    "     VALUES (:event_id, :tag, :value, :json_array)")
-    end
-
     def drop_tables
       %w[events tags subscriptions].each { |tbl|
         @db.execute "DROP TABLE IF EXISTS #{tbl}" rescue nil
@@ -254,9 +251,9 @@ module Nostrb
                                         sig BLOB NOT NULL)"
 
       # create index on pubkey, kind, created_at
-      @db.execute "CREATE INDEX pubkey_idx ON events (pubkey)"
-      @db.execute "CREATE INDEX kind_idx ON events (kind)"
-      @db.execute "CREATE INDEX created_at_idx ON events (created_at)"
+      @db.execute "CREATE INDEX idx_events_pubkey ON events (pubkey)"
+      @db.execute "CREATE INDEX idx_events_kind ON events (kind)"
+      @db.execute "CREATE INDEX idx_events_created_at ON events (created_at)"
 
       # primary key: (event_id, tag)
       @db.execute "CREATE TABLE tags (event_id    BLOB REFERENCES
@@ -266,6 +263,16 @@ module Nostrb
                                       json        TEXT NOT NULL,
                      CONSTRAINT       tags_pkey   PRIMARY KEY (event_id, tag))"
       @db.execute "CREATE TABLE subscriptions (id TEXT PRIMARY KEY NOT NULL)"
+    end
+
+    def prepare
+      @add_event =
+        @db.prepare("INSERT INTO events
+                          VALUES (:content, :kind, :pubkey,
+                                  :created_at, :id, :sig)")
+      @add_tag =
+        @db.prepare("INSERT INTO tags " +
+                    "     VALUES (:event_id, :tag, :value, :json_array)")
     end
 
     # a valid hash, as returned from SignedEvent.validate!
