ecc380322a40eb0eab15672c54303dc3de63ddcf
Rick Hull <rick.hull@gmail.com>
2024-09-29 17:37:15 -0400

    more straightforward approach

--- examples/client.rb
+++ examples/client.rb
@@ -5,34 +5,25 @@ require 'async/websocket/client'
 require 'nostrb/source'
 
 url = 'wss://localhost:7070'
+endpoint = Async::HTTP::Endpoint.parse(url)
 
 sk, pk = SchnorrSig.keypair
 src = Nostrb::Source.new(pk)
 
 puts "Relay URL: #{url}"
+puts "Pubkey: #{SchnorrSig.bin2hex pk}"
 puts "Enter messages to send to the relay (empty line to quit):"
 
-Async do |task|
-  endpoint = Async::HTTP::Endpoint.parse(url)
-
+Async do
   Async::WebSocket::Client.connect(endpoint) do |conn|
-    input_task = task.async do
-      while line = $stdin.gets
-        str = line.chomp
-        break if str.empty?
-        event = src.text_note(str).sign(sk)
-        json = Nostrb.json(Nostrb::Source.publish(event))
-        conn.write(json)
-        conn.flush
-      end
-      task.stop
-    end
-
-    while !input_task.completed?
+    while line = $stdin.gets and line != "\n"
+      event = src.text_note(line.chomp).sign(sk)
+      json = Nostrb.json(Nostrb::Source.publish(event))
+      conn.write(json)
+      conn.flush
       resp = conn.read
       puts resp.buffer
     end
-  ensure
-    input_task&.stop
+    conn.shutdown  # eliminate ECONNRESET on the server end
   end
 end
