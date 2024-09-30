2cb5ea54a7d64312a7565dd945811511b7c7702e
Rick Hull <rick.hull@gmail.com>
2024-09-30 17:00:58 -0400

    move lib code to examples/online.rb

--- lib/nostrb/client.rb
+++ lib/nostrb/client.rb
@@ -73,37 +73,3 @@ module Nostrb
     end
   end
 end
-
-if __FILE__ == $0
-  require 'set'
-  c = Nostrb::Client.new('wss://localhost:7070')
-  sk, pk = SchnorrSig.keypair
-  src = Nostrb::Source.new(pk)
-
-  tag = SchnorrSig.bin2hex Random.bytes(4)
-
-  p = src.profile(name: "testing-#{tag}", about: tag, picture: tag).sign(sk)
-  puts "profile: #{p}"
-  puts c.publish(p)
-
-  e = src.text_note('hello world').sign(sk)
-  puts "event: #{e}"
-  puts c.publish(e)
-
-  # who else is out there?
-  # subscribe to kind:0 events in the last year
-  # gather pubkeys
-
-  pubkeys = Set.new
-
-  f = Nostrb::Filter.new
-  f.add_kinds 0
-  f.since = Nostrb::Filter.ago(years: 1)
-
-  puts "filter: #{f}"
-  c.subscribe(f) { |e| pubkeys.add e.fetch('pubkey') }
-  p pubkeys.to_a
-
-  puts "close:"
-  c.close
-end
