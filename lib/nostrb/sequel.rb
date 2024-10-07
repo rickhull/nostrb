f0a1c58b1281087c49d59e2baf5a951270b9f98c
Rick Hull <rick.hull@gmail.com>
2024-10-07 13:09:37 -0400

    reduce sequel support to read-only

--- lib/nostrb/sequel.rb
+++ lib/nostrb/sequel.rb
@@ -3,9 +3,8 @@ require 'nostrb/sqlite'
 
 module Nostrb
   module Sequel
-    class Storage < SQLite::Storage
-      FILENAME = 'sequel.tmp.db'
-      TABLES = [:events, :tags, :r_events, :r_tags]
+    class Reader
+      TABLES = ['events', 'tags', 'r_events', 'r_tags']
 
       def self.schema_line(col, cfg)
         [col.to_s.ljust(9, ' '),
@@ -13,130 +12,6 @@ module Nostrb
         ].join("\t")
       end
 
-      def initialize(filename = FILENAME, set_pragmas: true)
-        @filename = filename
-        @db = ::Sequel.sqlite(@filename)
-        @db.transaction_mode = :immediate
-        self.set_pragmas if set_pragmas
-      end
-
-      def set_pragmas
-        @db.pool.available_connections.each { |s3db|
-          pragma = SQLite::Pragma.new(s3db)
-          PRAGMAS.each { |name, val| pragma.set(name, val) }
-        }
-        PRAGMAS.clone
-      end
-
-      def pragma_scalars
-        pragma = SQLite::Pragma.new(@db.pool.available_connections.sample)
-        SQLite::Pragma::SCALAR.map { |p|
-          val, enum = pragma.get(p), SQLite::Pragma::ENUM[p]
-          val = format("%i (%s)", val, enum[val]) if enum
-          format("%s: %s", p, val)
-        }
-      end
-
-      def setup
-        Setup.new(@filename)
-      end
-
-      def reader
-        Reader.new(@filename)
-      end
-
-      def writer
-        Writer.new(@filename)
-      end
-
-      def schema(table)
-        @db.schema(table).map { |a| Storage.schema_line(*a) }
-      end
-
-      def report
-        lines = []
-        TABLES.each { |t|
-          lines << t
-          lines += schema(t)
-          lines << ''
-        }
-        lines += self.pragma_scalars
-        lines
-      end
-    end
-
-    class Setup < Storage
-      def setup
-        drop_tables
-        create_tables
-        report
-      end
-
-      def drop_tables
-        @db.drop_table?(*TABLES)
-      end
-
-      def create_tables
-        @db.create_table :events, strict: true do
-          text :content,    null: false
-          int  :kind,       null: false
-          text :tags,       null: false
-          text :pubkey,     null: false
-          int  :created_at, null: false, index: {
-                 name: :idx_events_created_at
-               }
-          text :id,         null: false, primary_key: true
-          text :sig,        null: false
-        end
-
-        @db.create_table :tags, strict: true do
-          foreign_key :event_id, :events,
-                      key: :id,
-                      type: :text,
-                      null: false,
-                      on_delete: :cascade,
-                      on_update: :cascade
-          int  :created_at, null: false, index: { name: :idx_tags_created_at }
-          text :tag,        null: false
-          text :value,      null: false
-          text :json,       null: false
-        end
-
-        @db.create_table :r_events, strict: true do
-          text :content,    null: false
-          int  :kind,       null: false
-          text :tags,       null: false
-          text :d_tag,      null: true, default: nil
-          text :pubkey,     null: false
-          int  :created_at, null: false, index: {
-                 name: :idx_r_events_created_at
-               }
-          text :id,         null: false, primary_key: true
-          text :sig,        null: false
-          index [:kind, :pubkey, :d_tag], {
-                  unique: true,
-                  name: :unq_r_events_kind_pubkey_d_tag,
-                }
-        end
-
-        @db.create_table :r_tags, strict: true do
-          foreign_key :r_event_id, :r_events,
-                      key: :id,
-                      type: :text,
-                      null: false,
-                      on_delete: :cascade,
-                      on_update: :cascade
-          int  :created_at, null: false, index: {
-                 name: :idx_r_tags_created_at
-               }
-          text :tag,        null: false
-          text :value,      null: false
-          text :json,       null: false
-        end
-      end
-    end
-
-    class Reader < Storage
       def self.hydrate(event_row)
         Hash[ 'content' => event_row.fetch(:content),
               'kind' => event_row.fetch(:kind),
@@ -156,6 +31,42 @@ module Nostrb
         b = filter.until || Time.now.to_i
         hsh[:created_at] = a..b
         hsh
+      end
+
+      attr_reader :filename, :db, :pragma_scalars
+
+      def initialize(filename = SQLite::Storage::FILENAME)
+        @filename = filename
+        @db = ::Sequel.sqlite(filename)
+        @db.transaction_mode = :immediate
+        @db.pool.all_connections { |s3db|
+          # set performance pragmas
+          pragma = SQLite::Pragma.new(s3db)
+          SQLite::Storage::PRAGMAS.each { |name, val|
+            pragma.set(name, val)
+          }
+          # store all current pragma values (scalars only, not reports)
+          @pragma_scalars ||= SQLite::Pragma::SCALAR.map { |p|
+            val, enum = pragma.get(p), SQLite::Pragma::ENUM[p]
+            val = format("%i (%s)", val, enum[val]) if enum
+            format("%s: %s", p, val)
+          }
+        }
+      end
+
+      def schema(table)
+        @db.schema(table).map { |a| Reader.schema_line(*a) }
+      end
+
+      def report
+        lines = []
+        TABLES.each { |t|
+          lines << t
+          lines += schema(t)
+          lines << ''
+        }
+        lines += @pragma_scalars
+        lines
       end
 
       def select_events_table(table = :events, filter = nil)
@@ -184,37 +95,6 @@ module Nostrb
         a = []
         select_r_events(filter).each { |row| a << Reader.hydrate(row) }
         a
-      end
-    end
-
-    class Writer < Storage
-      def add_event(valid)
-        @db[:events].insert(valid.merge('tags' => Nostrb.json(valid['tags'])))
-        valid['tags'].each { |a|
-          @db[:tags].insert(event_id: valid['id'],
-                            created_at: valid['created_at'],
-                            tag: a[0],
-                            value: a[1],
-                            json: Nostrb.json(a))
-        }
-      end
-
-      # use insert_conflict to replace latest event
-      def add_r_event(valid)
-        tags = valid.fetch('tags')
-        d_tag = Event.d_tag(tags)
-        hsh = {
-          'tags' => Nostrb.json(tags),
-          'd_tag' => d_tag,
-        }
-        @db[:r_events].insert_conflict.insert(valid.merge(hsh))
-        valid['tags'].each { |a|
-          @db[:r_tags].insert(r_event_id: valid['id'],
-                              created_at: valid['created_at'],
-                              tag: a[0],
-                              value: a[1],
-                              json: Nostrb.json(a))
-        }
       end
     end
   end
